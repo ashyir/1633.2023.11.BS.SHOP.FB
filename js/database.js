2a0bdb571b4ac33cdec92404267b1575f81b31ee
Serco Honan <ashyir@yahoo.com>
2023-11-26 16:03:22 +0700

    Finish login, register, product.

--- js/database.js
+++ js/database.js
@@ -1,20 +1,106 @@
-const sessionCarts = 'carts';
-const sessionProducts = 'products';
-const sessionAccounts = 'accounts';
-const sessionLoginEmail = 'login_email';
+const dbCartName = 'carts';
+const dbUserName = 'users';
+const dbProductName = 'products';
+const sessionLoginUser = 'loginUser';
 
-function getSessionLoginEmail() {
-    return JSON.parse(sessionStorage.getItem(sessionLoginEmail));
+const firebaseConfig = {
+    apiKey: 'AIzaSyBPKrkOWpiwO2wH_gb5w51k1d5bngswEYA',
+    authDomain: 'fgw-1633.firebaseapp.com',
+    projectId: 'fgw-1633',
+    storageBucket: 'fgw-1633.appspot.com',
+    messagingSenderId: '194069942003',
+    appId: '1:194069942003:web:63abd84ef7b6a22b283186'
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+function getSessionLoginUser() {
+    return JSON.parse(sessionStorage.getItem(sessionLoginUser));
 }
 
-function setSessionLoginEmail(email) {
-    sessionStorage.setItem(sessionLoginEmail, JSON.stringify(email));
+function setSessionLoginUser(user) {
+    sessionStorage.setItem(sessionLoginUser, JSON.stringify(user));
 }
 
-function removeSessionLoginEmail() {
-    sessionStorage.removeItem(sessionLoginEmail);
+function removeSessionLoginUser() {
+    sessionStorage.removeItem(sessionLoginUser);
 }
 
+function dbGetProducts() {
+    return new Promise((resolve, reject) => {
+        db.collection(dbProductName)
+            .get()
+            .then((querySnapshot) => {
+                const products = [];
+
+                querySnapshot.forEach((doc) => {
+                    const productData = doc.data();
+
+                    const product = {
+                        id: doc.id,
+                        name: productData.name,
+                        price: productData.price,
+                        image: productData.image,
+                        description: productData.description,
+                    };
+
+                    products.push(product);
+                });
+
+                resolve(products);
+            })
+            .catch((error) => {
+                console.error('Error getting products:', error);
+                reject(error);
+            });
+    });
+}
+
+function dbAddProduct(product) {
+    return new Promise((resolve, reject) => {
+        db.collection(dbProductName)
+            .add(product)
+            .then(() => { resolve(true); })
+            .catch((error) => {
+                console.error('Error adding product:', error);
+                reject(false);
+            });
+    });
+}
+
+function dbAddUser(user) {
+    return new Promise((resolve, reject) => {
+        firebase.auth()
+            .createUserWithEmailAndPassword(user.email, user.password)
+            .then((userCredential) => {
+                userCredential.user.updateProfile({
+                    displayName: user.displayName,
+                    photoURL: user.photoURL,
+                });
+
+                resolve(userCredential.user);
+            })
+            .catch((error) => {
+                console.error('Error creating user account:', error);
+                reject(error);
+            });
+    });
+}
+
+function dbAuthenticate(email, password) {
+    return new Promise((resolve, reject) => {
+        firebase.auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((userCredential) => { resolve(userCredential.user); })
+            .catch((error) => {
+                console.error('Authentication failed:', error);
+                reject(error);
+            });
+    });
+}
+
+/*
 function getSessionCarts() {
     return JSON.parse(sessionStorage.getItem(sessionCarts)) || [];
 }
@@ -27,29 +113,4 @@ function getSessionCart(email) {
     const carts = getSessionCarts();
     return carts.find((item) => item.email === email);
 }
-
-function getSessionProducts() {
-    return JSON.parse(sessionStorage.getItem(sessionProducts)) || [];
-}
-
-function setSessionProducts(products) {
-    sessionStorage.setItem(sessionProducts, JSON.stringify(products));
-}
-
-function getSessionProduct(product_id) {
-    const products = JSON.parse(sessionStorage.getItem(sessionProducts)) || [];
-    return products.find((item) => item.id === product_id);;
-}
-
-function getSessionAccounts() {
-    return JSON.parse(sessionStorage.getItem(sessionAccounts)) || [];
-}
-
-function setSessionAccounts(accounts) {
-    sessionStorage.setItem(sessionAccounts, JSON.stringify(accounts));
-}
-
-function authenticate(email, password) {
-    const accounts = getSessionAccounts();
-    return accounts.some(item => item.email === email && item.password === password);
-}
+*/
--- js/navigation.js
+++ js/navigation.js
@@ -20,12 +20,12 @@ function displayMenu() {
 }
 
 function isLogout() {
-    const email = getSessionLoginEmail();
-    return email === null;
+    const user = getSessionLoginUser();
+    return user === null;
 }
 
 function logout() {
-    removeSessionLoginEmail();
+    removeSessionLoginUser();
     location.reload();
 }
 
@@ -69,7 +69,6 @@ function setMenuItemActive(pageName) {
 }
 
 function removeMenuItemActive() {
-    if (document.querySelector('.nav-item a.active') !== null) {
+    if (document.querySelector('.nav-item a.active') !== null)
         document.querySelector('.nav-item a.active').classList.remove('active');
-    }
 }
